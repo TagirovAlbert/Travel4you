688079e81c95493f5401a44f2137df1ee2722cb8
TagirovAlbert <bert_tagirov@mail.ru>
2015-12-08 18:44:25 +0300

    Add vk twitter config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -229,7 +229,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-   config.omniauth :facebook, ENV["FACEBOOK_API_KEY"], ENV["FACEBOOK_API_SECRET"]
+  config.omniauth :facebook, ENV["FACEBOOK_API_KEY"], ENV["FACEBOOK_API_SECRET"]
+  config.omniauth :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"]
+  config.omniauth :vkontakte, ENV["VKONTAKTE_API_KEY"], ENV["VKONTAKTE_API_SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
