95ad698edce8f336d31a7fec3bfa2e6231f2be5b
Albert Tagirov <bert_tagirov@mail.ru>
2016-06-01 19:16:35 +0300

    true routes in country-region-city

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def profile
+
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-  resources :photo_reports do
-    resources :images, :only => [:create, :destroy]
-  end
   resources :places
-  resources :cities do
-    collection do
-      get 'get_regions', to: "cities#get_regions"
+  resources :countries do
+    resources :regions do
+      resources :cities do
+        resources :photo_reports do
+          resources :images, :only => [:create, :destroy]
+        end
+
+      end
     end
   end
-  resources :countries
-  resources :regions
-
   match '/test', to: 'countries#test', via: 'get'
   match '/about', to: 'static_pages#about_us', via: 'get', :as => :about
-devise_for :users, controllers: { omniauth_callbacks: 'omniauth/omniauth_callbacks', registrations: 'registrations' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth/omniauth_callbacks', registrations: 'registrations' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch, :put], :as => :finish_signup
   root to: "pages#home"
 end
