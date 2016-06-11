07804b4c66c54f6091c9bd602c3e3ce5b75dd14f
Albert Tagirov <bert_tagirov@mail.ru>
2016-06-11 03:10:39 +0300

    update decorators(user^ photo_reports)

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,9 +1,14 @@
-class UserDecorator < ApplicationDecorator
+class UserDecorator < Draper::Decorator
   delegate :id, :first_name,:last_name, :email, :login, :identities
 
   def full_name_with_email
     "#{object.first_name} #{object.last_name} (#{object.email})"
   end
+
+  def full_name
+    "#{object.first_name} #{object.last_name}"
+  end
+
 
   def not_have_ident?
     object.identities.first.nil?
