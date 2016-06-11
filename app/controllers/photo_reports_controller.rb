ef8dcb60debfd120e9b7255ca0e242911e3c9d2f
Albert Tagirov <bert_tagirov@mail.ru>
2016-06-11 03:23:06 +0300

    create photo_report and places only after authorization

--- app/controllers/photo_reports_controller.rb
+++ app/controllers/photo_reports_controller.rb
@@ -1,12 +1,13 @@
 class PhotoReportsController < ApplicationController
   respond_to :html
-
+  before_action :authenticate_user!, :except => [:show, :index]
   expose(:photo_report, attributes: :photo_report_params)
   expose(:photo_reports) { PhotoReport.page(params[:page]) }
 
   def create
     city = City.find_by_id(photo_report_params[:city_id])
     country = city.country
+    photo_report.user =current_user
     photo_report.city = city
     photo_report.country = country
     flash[:notice] = 'Photo report was successfully created.' if photo_report.save
@@ -15,11 +16,16 @@ class PhotoReportsController < ApplicationController
 
   def index
     @photo_reports = PhotoReport.all.order("created_at DESC")
+    @city_of_report = Array.new
+    for elem in @photo_reports
+      @city_of_report.append(elem.city.name)
+    end
   end
 
   def show
     photo_report.increment(:visitors)
     photo_report.save
+    @user = photo_report.user.decorate
   end
 
   def update
